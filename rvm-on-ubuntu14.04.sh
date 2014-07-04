806b09bcb09e286a4d136c9a279a5e293c0d507d
G SubbaRao <subbarao.kly@gmail.com>
2014-07-04 23:31:03 +0530

    Required ubuntu packages through shell scripts

---
+++ rvm-on-ubuntu14.04.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config libgdbm-dev libffi-dev libreadline-dev
+curl -L https://get.rvm.io | bash -s stable
+echo ‘source ~/.rvm/scripts/rvm’ » ~/.bash_aliases && bash
+rvm install 2.0.0-p247
+
